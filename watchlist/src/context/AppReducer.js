f48c018189e5fb4c187d7f4877654516be7e3aef
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2021-12-21 20:36:01 +0100

    saving on github

--- watchlist/src/context/AppReducer.js
+++ watchlist/src/context/AppReducer.js
@@ -1,6 +1,6 @@
 export default (state, action) => {
     switch (action.type){
-        case "ADD_MOVIE_TO_WWATCHLIST":
+        case "ADD_MOVIE_TO_WATCHLIST":
             return {
                 ...state,
                 watchlist: [action.payload, ...state.watchlist],
--- watchlist/src/context/GlobalState.js
+++ watchlist/src/context/GlobalState.js
@@ -18,13 +18,13 @@ export const GlobalProvider = (props) =>{
 
 
     // actions
-    const addMovieWatchList = movie =>{
+    const addMovieToWatchlist = movie =>{
         dispatch({type: "ADD_MOVIE_TO_WATCHLIST", payload: movie})
     }
 
 
     return (
-        <GlobalContext.Provider value={{watchlist :state.watchlist,watched : state.watched, addMovieWatchList,}}>
+        <GlobalContext.Provider value={{watchlist :state.watchlist,watched : state.watched, addMovieToWatchlist,}}>
             {props.children}
         </GlobalContext.Provider>
     )
