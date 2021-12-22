6dfe94c77876ed163acf41f885f442218779eed3
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2021-12-22 21:51:08 +0100

    saving on github

--- watchlist/src/context/GlobalState.js
+++ watchlist/src/context/GlobalState.js
@@ -3,9 +3,10 @@ import AppReducer from './AppReducer';
 
 //initial state
 const initialState = {
-    watchlist:[],
-    watched :[],
-};
+    watchlist: localStorage.getItem("watchlist") ? JSON.parse(localStorage.getItem("watchlist")) : [],
+    watched: localStorage.getItem("watched") ? JSON.parse(localStorage.getItem("watched")) : [],
+
+}
 
 // create context
 
@@ -16,6 +17,12 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = (props) =>{
     const [state,dispatch] = useReducer(AppReducer, initialState);
 
+
+
+    useEffect(() => {
+        localStorage.setItem("watchlist" , JSON.stringify(state.watchlist))
+        localStorage.setItem("watched" , JSON.stringify(state.watched))
+    }, [state])
 
     // actions
     const addMovieToWatchlist = movie =>{
